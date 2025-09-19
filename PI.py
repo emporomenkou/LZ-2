8dbcd93a4d75096f5bdd001d6fc70d222de36adc
emporomenkou <noobprohacker@tutamail.com>
2025-09-19 13:09:50 +0300

    PI

---
+++ PI.py
@@ -0,0 +1,6 @@
+import math
+r = float(input ('Введите радиус:'))
+circle_length = 2 * r * math.pi
+square = r ** 2 * math.pi
+print ("Длина окружности:", circle_length)
+print ("Площадь круга:", square)
---
+++ second.py
@@ -0,0 +1,9 @@
+# import random 
+# start = int(10)
+# end = int(1000)
+# random_number = random.randint(start, end)
+a = 6
+b = 8.3
+square = a * b
+print (square)
+# print (random_number)
